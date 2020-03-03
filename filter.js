0856178d8ce2eff2b32914c802dfe53dfc126c5b
Nilo Rocha <nilsr44@gmail.com>
2020-03-03 21:21:24 +0100

    ok

--- filter.js
+++ filter.js
@@ -1,1 +1,23 @@
-
+let vetor1 = [0, 1 , 2, 3, 4, 5, 6, 7, 8, 9]
+let numeroPar = []
+for (let i = 0; i < vetor1.length; i++){
+    if (vetor1[i] % 2 === 0){
+        numeroPar.push(vetor1[i])
+    }
+}
+console.log(numeroPar)
+let soma = 0
+for(let i = 0; i < numeroPar.length; i++){
+    soma += numeroPar[i]
+}
+console.log(soma)
+//
+//
+//
+//
+let vetor2 = [0, 1 , 2, 3, 4, 5, 6, 7, 8, 9]
+let numerosParesFiltrados = vetor2.filter(valor => valor % 2 === 0)
+console.log(numerosParesFiltrados)
+let somaVetor = numerosParesFiltrados.reduce((Acumulador, valorVetor) => Acumulador + valorVetor
+, 0)
+console.log(somaVetor)
--- generator.js
+++ generator.js
@@ -1,1 +1,4 @@
-
+let gerador = function(...tranformar){
+    return console.log(tranformar)
+}
+gerador(1, 3, 5, 7, 9)
